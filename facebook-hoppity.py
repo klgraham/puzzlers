4787c85791eb45892a187833a55123f867557c0e
Ken Graham <klgraham@gmail.com>
2011-06-17 16:06:27 -0500

    removed extra space

--- facebook-hoppity.py
+++ facebook-hoppity.py
@@ -12,18 +12,20 @@ infile = open(r'hoppity_input.txt','r')
 iterationString = infile.read()
 
 # strip newline, then strip padding spaces, then cast to integer
-n = int( ( iterationString.strip('\n') ).strip(' ') )
+n = int((iterationString.strip('\n')).strip(' '))
 
 # dictionary of possible results
 output = {
-  (True,False): 'Hoppity',
-  (False,True): 'Hophop',
-  (True,True): 'Hop'
+    (True,False): 'Hoppity',
+    (False,True): 'Hophop',
+    (True,True): 'Hop'
 }
  
 for x in range(1,n):
-  # evenly divisible by three or five?
-  multipleOfThree = bool( x % 3 == 0)
-  multipleOfFive = bool( x % 5 == 0)
-  result = output.get((multipleOfThree,multipleOfFive))
-  if bool(result): print(result)
+    # evenly divisible by three or five?
+    multipleOfThree = bool(x % 3 == 0)
+    multipleOfFive = bool(x % 5 == 0)
+  
+    result = output.get((multipleOfThree,multipleOfFive))
+    if bool(result):
+        print(result)
